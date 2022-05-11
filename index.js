c50680b96f692159c3c7812afe4dc3803f327973
Adam Clingmon <98420991+asocrazea@users.noreply.github.com>
2022-05-11 10:04:14 -0700

    create index.js
    created

---
+++ index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Front-end foundations: Responsive images</title>
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
