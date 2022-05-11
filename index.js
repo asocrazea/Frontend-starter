188c565b18f6a7d84c9b2ff08c1ead91ac12781a
Adam Clingmon <98420991+asocrazea@users.noreply.github.com>
2022-05-11 10:05:13 -0700

    update
    added forms

--- index.js
+++ index.js
@@ -11,3 +11,56 @@
     />
     <link href="style.css" rel="stylesheet" type="text/css" />
   </head>
+<body>
+    <main>
+      <h1>Important Page Heading</h1>
+      <section>
+        <h2>Group #1 Heading</h2>
+        <!-- Group #1 with 3 items -->
+        <div class="group">
+          <div class="item">
+            <img src="images/watermelon1.jpg" alt="" />
+          </div>
+          <div class="item">
+            <img src="images/watermelon2.jpg" alt="" />
+          </div>
+          <div class="item">
+            <img src="images/watermelon3.jpg" alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <h2>Group #2 Heading</h2>
+        <!-- Group #2 with 2 items -->
+        <div class="group">
+          <div class="item">
+            <img src="images/pineapple.jpg" alt="" />
+          </div>
+          <div class="item">
+            <img src="images/icecream.jpg" alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <h2>Group #3 Heading</h2>
+        <!-- Group #3 with 4 items -->
+        <div class="group">
+          <div class="item">
+            <img src="images/water1.jpg" alt="" />
+          </div>
+          <div class="item">
+            <img src="images/water2.jpg" alt="" />
+          </div>
+          <div class="item">
+            <img src="images/water3.jpg" alt="" />
+          </div>
+          <div class="item">
+            <img src="images/water4.jpg" alt="" />
+          </div>
+        </div>
+      </section>
+    </main>
+  </body>
+</html>
